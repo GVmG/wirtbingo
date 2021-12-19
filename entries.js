3d9c522fb1fab8a561f2a78ee524e984fbe2cfa7
tomasitobambito <tomgacek0604@gmail.com>
2021-12-19 13:49:41 +0100

    sorted entries into categories for easier editing and better overview

--- entries.js
+++ entries.js
@@ -1,23 +1,16 @@
 const bingos=[
+// Text only
 "Pulls a Maji",
 "Sound alert at bathroom break",
 "Single digit COTD Div",
-"<img src=\"https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_bcd77a288fa24c90855a34965af39f68/default/light/1.0\" /> Maji Rage",
-"<img src=\"https://cdn.betterttv.net/emote/5f1b0186cf6d2144653d2970/1x\" /> banger song",
 "> 10 First round KO in Cup of the Maji",
 "Chat asks what Maji is drinking",
 "Maji drinks Saft without diluting it first",
 "JUST DO IT",
 "Storytime",
-"<img src=\"https://cdn.frankerfacez.com/emoticon/418189/1\" /> POLE",
-"<img src=\"https://cdn.frankerfacez.com/emoticon/418189/1\" /> TREE",
-"<img src=\"https://cdn.frankerfacez.com/emoticon/418189/1\" /> COCK",
-"<img src=\"https://cdn.frankerfacez.com/emoticon/418189/1\" /> OIL",
 "COTD V2",
 "Maji talks about anime",
-"<img src=\"https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_19cc0f6103d94ea0b0bb27ab270cf991/default/light/1.0\" /> Maji says something sus (Free Space)",
 "Maji roasts someone",
-"<img src=\"https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_b5c66ddbb1cd4aa8b89c372fcbb8ff30/static/light/1.0\" /> Chair gang redeem",
 "COTD Win (in any div)",
 "Speaks Norwegian",
 "Maji builds a loop",
@@ -29,9 +22,21 @@ const bingos=[
 "Ice Map",
 "Word lost in translation",
 "VC with someone",
-"Chat is <img src=\"https://cdn.frankerfacez.com/emoticon/243789/1\" />",
+
+// Non-seasonal emotes
+"<img src=\"https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_bcd77a288fa24c90855a34965af39f68/default/light/1.0\" /> Maji Rage",
+"<img src=\"https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_b5c66ddbb1cd4aa8b89c372fcbb8ff30/static/light/1.0\" /> Chair gang redeem",
+"<img src=\"https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_5b1982e6fb094bd6898a6deedcb0d6d7/default/light/1.0\" /> Maji threatens someone",
+"<img src=\"https://cdn.betterttv.net/emote/5f1b0186cf6d2144653d2970/1x\" /> banger song",
 "<img src=\"https://cdn.betterttv.net/emote/617706061f8ff7628e6afb02/1x\" /> Streamer doesn't know",
 "Chat spams <img src=\"https://cdn.betterttv.net/emote/60cd2cfb8ed8b373e4216130/1x\" />",
-"<img src=\"https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_5b1982e6fb094bd6898a6deedcb0d6d7/default/light/1.0\" /> Maji threatens someone",
-"<img src=\"https://cdn.frankerfacez.com/emoticon/457124/1\" /> WICKED"
+"<img src=\"https://cdn.frankerfacez.com/emoticon/418189/1\" /> POLE",
+"<img src=\"https://cdn.frankerfacez.com/emoticon/418189/1\" /> TREE",
+"<img src=\"https://cdn.frankerfacez.com/emoticon/418189/1\" /> COCK",
+"<img src=\"https://cdn.frankerfacez.com/emoticon/418189/1\" /> OIL",
+"<img src=\"https://cdn.frankerfacez.com/emoticon/457124/1\" /> WICKED",
+"Chat is <img src=\"https://cdn.frankerfacez.com/emoticon/243789/1\" />",
+
+// Seasonal emotes
+"<img src=\"https://static-cdn.jtvnw.net/emoticons/v2/emotesv2_19cc0f6103d94ea0b0bb27ab270cf991/default/light/1.0\" /> Maji says something sus (Free Space)"
 ];
